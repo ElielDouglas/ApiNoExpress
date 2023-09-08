ff61c5ddad7fe9ffb812870b3ffb5975995baead
ElielDouglas <eliel238@hotmail.com>
2023-09-08 12:39:50 -0300

    added body parser

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,17 +3,21 @@ const users = require('../mocks/users')
 module.exports = {
   listUsers(req, res){
     const { order } = req.query
+
     const sortedUsers = users.sort((a,b) =>{
+
       if(order === 'desc'){
         return a.id < b.id ? 1 : -1;
       }
       return a.id > b.id ? 1 : -1;
     })
+
     res.send(200, sortedUsers);
   },
 
   getUserById(req, res){
     const { id } = req.params;
+
     const user = users.find((user) => user.id === Number(id))
 
     if(!user){
@@ -21,6 +25,17 @@ module.exports = {
     }
     
     res.send(200, user);
+  },
 
+  createUser(req, res){
+    const { body } = req
+    const lastUserId = users[users.length - 1].id; 
+    const newUser = {
+      id: lastUserId + 1,
+      name: body.name
+    };
+    users.push(newUser);
+
+    res.send(200, newUser)
   }
 };
